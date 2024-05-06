c72b7e17efd3cf14dc867d236c16752f7bb1d322
ShriyaSV <shriyashriya1976@gmail.com>
2024-05-06 14:02:38 +0530

    added files

---
+++ src/components/Testimonials/Testimonials.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Testimonials.css";
+const Testimonials = () => {
+  return (
+    <div className="Testimonials">
+        <div className="left-t">
+            <span>Testimonials</span>
+            <span className="stroke-text">What they</span>
+            <span>say about us</span>
+        </div>
+        <dic className="right-t"></dic>
+    </div>
+  )
+}
+
+export default Testimonials
